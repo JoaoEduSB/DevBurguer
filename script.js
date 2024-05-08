68867b38c4fffd367dbb2a747db23bb07f405965
João Eduardo <joao.eduardo.souza@hotmail.com>
2024-05-08 20:03:22 -0300

    Atualização do arquivo Javascript

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ checkoutBtn.addEventListener("click", function() {
 function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 20 && hora < 22;  // true (se for maior que 18 e menor que 22 restaurante está aberto)
+    return hora >= 18 && hora < 22;  // true (se for maior que 18 e menor que 22 restaurante está aberto)
 }
 
 const spanItem = document.getElementById("date-span")
